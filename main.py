a21ccb8ffe1440d945f91e6ff8de85c172c71c7d
Bhimani Darshan Jayshukhbhai <darshanbhimani2003@gmail.com>
2025-06-22 10:34:00 +0530

    email sending refector

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ app.openapi = custom_openapi
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-documentgenerator.vercel.app"], 
-    # allow_origins=["http://localhost:3000"], 
+    # allow_origins=["https://ai-documentgenerator.vercel.app"], 
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
